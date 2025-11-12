aae2b6737c00647bc9ed1b50b25b67c352e095d2
Alejandro Agra <aleagra08@hotmail.com>
2025-11-12 19:53:51 -0300

    navbar functionalities

--- therapify/src/app/navbar/navbar.component.ts
+++ therapify/src/app/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   menuOpen = false;
+  isLogged = signal(false);
+
+  constructor(private router: Router) {
+    const user = localStorage.getItem('userLogged');
+    this.isLogged.set(!!user);
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  goToProfile() {
+    if (this.isLogged()) {
+      this.router.navigate(['/profile']);
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
 }
