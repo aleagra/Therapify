695befe15014d7fdf7aff060aa92a335e2efa1b9
Alejandro Agra <aleagra08@hotmail.com>
2025-11-11 23:09:25 -0300

    styles

--- therapify/src/app/app.component.ts
+++ therapify/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'therapify';
