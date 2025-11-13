32245be38f79e8533e03efac5d4e7efe5453abfa
Alejandro Agra <aleagra08@hotmail.com>
2025-11-13 19:31:23 -0300

    Update app.component.ts

--- therapify/src/app/app.component.ts
+++ therapify/src/app/app.component.ts
@@ -1,39 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core'; // Adaptador de fecha nativo
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle'; // Genial para horarios
-import { MatCardModule } from '@angular/material/card'; // Para el resumen
-import { MatDividerModule } from '@angular/material/divider';
-import { MatIconModule } from '@angular/material/icon';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatDividerModule,
-    MatIconModule,
-    RouterOutlet,
-  ],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
