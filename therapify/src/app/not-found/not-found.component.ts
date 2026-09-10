7b209abc70b5402b73ff405d5c3b00598cb8c6d2
Alejandro Agra <aleagra08@hotmail.com>
2026-09-10 12:17:18 -0300

    feat(design-system): auth centrado verticalmente, banner 620px, wizard de registro e ilustraciones vectoriales

--- therapify/src/app/not-found/not-found.component.ts
+++ therapify/src/app/not-found/not-found.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-not-found',
-  imports: [RouterLink],
+  imports: [RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css',
 })
